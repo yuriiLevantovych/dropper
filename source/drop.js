3db40af801297bbee5e085fdc2bbf845caee24b3
domovoj <levantovychbogdan@gmail.com>
2014-11-03 21:58:46 +0200

    front

--- source/drop.js
+++ source/drop.js
@@ -742,7 +742,7 @@
                     n = n.split('{')[0];
                     text = text.replace(n, n.replace(/,(?!(\s*\[drop\])|(\s*\[\[))/g, ', ' + opt.tempClassS + ' '));
                 });
-            text = text.replace(/\}[^$](?!(\s*\[drop\])|(\s*\[\[))/g, '} ' + opt.tempClassS + ' ').replace(/^(?!(\s*\[drop\])|(\s*\[\[))/, opt.tempClassS + ' ').replace(/\[\[(.*?)\]\]/g, '$1').replace(/\[drop\]/g, opt.tempClassS).replace(/\s\s+/g, ' ');
+            text = text.replace(/\}[^$](?!(\s*\[drop\])|(\s*\[\[))/g, '} ' + opt.tempClassS + ' ').replace(/^(?!(\s*\[drop\])|(\s*\[\[))/, opt.tempClassS + ' ').replace(/\[\[(.*?)\]\]/g, '$1').replace(/\[drop\]/g, opt.tempClassS).replace(/\s{2,}/g, ' ');
             return $('<style>', {
                 'data-rel': opt.tempClassS,
                 html: text
@@ -941,8 +941,8 @@
         },
         theme: {
             default: '.drop-header{background-color: #f8f8f8;padding: 0 55px 0 12px;font-size: 14px;}\n\
-                    input, textarea{margin-bottom: 6px;}\n\
-                    input, textarea, .drop-content button{outline: none;font-family: Arial, Helvetica CY, Nimbus Sans L, sans-serif;line-height: 1.5;border: 1px solid #d8d8d8;padding: 4px 6px;}\n\
+                    input, select, textarea{margin-bottom: 6px;}\n\
+                    input, select, textarea, .drop-content button{outline: none;font-family: Arial, Helvetica CY, Nimbus Sans L, sans-serif;line-height: 1.5;border: 1px solid #d8d8d8;padding: 4px 6px;}\n\
                     button{background-color: #fafafa;box-shadow: inset 0 1px #fefefe;color: #666;cursor: pointer;}\n\
                     .drop-header.drop-no-empty, .drop-footer.drop-no-empty{padding-top: 6px;padding-bottom: 7px;}\n\
                     .drop-header.drop-no-empty{border-bottom: 1px solid #d8d8d8;}\n\
@@ -953,7 +953,7 @@
                     [[.drop-image .drop-content .inside-padd]], [[.drop-alert .drop-content .inside-padd]]{padding: 10px;}\n\
                     [[.drop-alert .drop-group-btns]]{text-align: center;}\n\
                     .drop-content button{margin-right: 4px;}{\n\
-                    button:focus, input:focus, textarea:focus{outline: #b3b3b3 solid 1px;}\n\
+                    button:focus, input:focus, textarea:focus, select:focus{outline: #b3b3b3 solid 1px;}\n\
                     .drop-footer{background-color: #d5d5d5;padding: 0 12px;}\n\
                     .drop-close, .drop-prev, .drop-next{outline: none;background: none;border: 0;cursor: pointer;vertical-align: middle;position: absolute;font-size: 0;padding: 0;}\n\
                     .drop-prev, .drop-next{width: 35%;height: 100%;top: 0;z-index: 2;}\n\
